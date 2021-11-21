bfb58efbc307f43be3ebdc9ff6ab4a946f3d5dad
ragna <sir.ragna@gmail.com>
2021-11-21 14:57:02 +0100

    save record to database

--- tutorial1/phonebook/views.py
+++ tutorial1/phonebook/views.py
@@ -5,8 +5,15 @@ from django.shortcuts import render
 from phonebook.models import PhoneRecord
 
 def new_phone_record(request):
-    # TODO Save phone record
+    if request.method == 'POST':
+        if "name" in request.POST and "number" in request.POST:
+            name = request.POST["name"]
+            number = request.POST["number"]
+            new_phone_record = PhoneRecord(name=name, number=number)
+            new_phone_record.save()
     
+    # TODO user feedback about success or failure of saving the record
+
     # redirect to the main page
     return HttpResponseRedirect(reverse('index'))
 
@@ -23,6 +30,7 @@ def index(request):
     # new_phone_record.save()
 
     all_records = PhoneRecord.objects.all()
+    #PhoneRecord.objects.filter(name="john")
 
     return render(request, "frontpage.html.j2", 
         { "phone_records" : all_records }
